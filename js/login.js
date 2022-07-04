b1493ffacf9926a8eef825f046bb24de33aa3051
Daniel Leonel <eddfriends@hotmail.com>
2022-07-04 05:39:58 -0500

    Se modificarón los arcivhos de inicio-sesion.html y sesion.js para validaciones e inicio de sesión con datos en el localStorage

--- js/login.js
+++ js/login.js
@@ -15,9 +15,27 @@ form.addEventListener('submit', (e) =>{
         password: data.get('password'),
         id: `${Date.now()}`
     }
+    espacios=false;
+    stringEmail= objectUser.email;
+    stringPassword = objectUser.password;
+    for (let index = 0; index < stringEmail.length; index++) {
+        if(stringEmail[index]==" "){
+            console.log("EL usuario lleva espacios");
+            espacios = true;
+        }
+    }
+    
+    for (let index = 0; index < stringPassword.length; index++) {
+        if(stringPassword[index]==" "){
+            console.log("La contraseña lleva espacios");
+            espacios = true;
+        }
+        
+        
+    }
 
     //Validacion para que el user no mande un input vacio
-    if (!data.get('email').trim() || !data.get('password').trim()) {
+    if (!data.get('email').trim() || !data.get('password').trim()|| espacios) {
          console.log("Mandaste un form vacio");
          alert.classList.remove('d-none');
          return;
@@ -25,8 +43,30 @@ form.addEventListener('submit', (e) =>{
     users.push(objectUser);
     //Guadar en localSorage
     localStorage.setItem("Users", JSON.stringify(users));
+    registros = JSON.parse(localStorage.getItem('Users'));
+    console.log(objectUser.email);
+    console.log(registros[0].email);
+    console.log(objectUser.password);
+    console.log(registros[0].password);
+    
+    console.log(registros[0].email)
+    if ((objectUser.email == registros[0].email)&&(objectUser.password==registros[0].password)) {
+        Swal.fire(
+            '¡Inicio de sesión aceptada.!',
+            '',
+            'success'
+          );
+    } else{
+        Swal.fire(
+            '¡Error en el usuario o contraseña.!',
+            '',
+            'success'
+          );
+
+    }
     console.log(users);
     form.reset();
+
 });
 
 //Obtener datos de localStorage
@@ -36,3 +76,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         console.log("Datos desde localStorage", users)
     }
 });
+
+document.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    usuario = localStorage.getItem
+})
